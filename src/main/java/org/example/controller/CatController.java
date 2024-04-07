697beced0de051bc9bb4e0ee5b8d3fdd6472f664
Alexandr Soldatov <asoldatov@vp.ru>
2024-04-07 23:38:27 +0300

    initial

---
+++ src/main/java/org/example/controller/CatController.java
@@ -0,0 +1,27 @@
+package org.example.controller;
+
+import org.example.service.CatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CatController {
+
+
+    private final CatService catService;
+
+    public CatController(CatService catService) {
+        this.catService = catService;
+    }
+
+
+}
